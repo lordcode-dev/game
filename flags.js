17c9bc10806e13037d327181ddb33d64d9b5be1c
lordcode-dev <mrbeast765389@gmail.com>
2026-02-21 23:25:41 +0300

    Add FLAGS array with country images

---
+++ flags.js
@@ -0,0 +1,12 @@
+const FLAGS = [
+  {country:"Japan",img:"https://flagcdn.com/w320/jp.png"},
+  {country:"Brazil",img:"https://flagcdn.com/w320/br.png"},
+  {country:"Canada",img:"https://flagcdn.com/w320/ca.png"},
+  {country:"Germany",img:"https://flagcdn.com/w320/de.png"},
+  {country:"Kenya",img:"https://flagcdn.com/w320/ke.png"},
+  {country:"India",img:"https://flagcdn.com/w320/in.png"},
+  {country:"France",img:"https://flagcdn.com/w320/fr.png"},
+  {country:"Italy",img:"https://flagcdn.com/w320/it.png"},
+  {country:"Ethiopia",img:"https://flagcdn.com/w320/et.png"},
+  {country:"USA",img:"https://flagcdn.com/w320/us.png"}
+];
